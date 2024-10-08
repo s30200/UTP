bbb75898b13de1e7732decdeb80e0a1ec37d1c23
s30200 <s30200@pjwstk.edu.pl>
2024-10-08 11:00:50 +0200

    initial project setup

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Adder adder = new Adder();
+        System.out.println(adder.add(1, 2));
+
+        Subtractor subtractor = new Sybtractor();
+        System.out.println(subtractor.subtract(6,3));
+    }
+}
